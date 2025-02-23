9ef08782ac8554572b10e92f78e8f6e8b1bef50d
Pedro Vinicius <pvinicius2005@hotmail.com>
2025-02-23 14:30:54 -0300

    initial commit

---
+++ src/main/java/com/pedroribeiro/helpaai/HelpaaiApplication.java
@@ -0,0 +1,13 @@
+package com.pedroribeiro.helpaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpaaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpaaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroribeiro/helpaai/HelpaaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroribeiro.helpaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpaaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
