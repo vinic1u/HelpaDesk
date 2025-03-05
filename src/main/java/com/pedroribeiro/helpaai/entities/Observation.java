39c2c281ab1f581a234e4390e8d189a5e6055d75
Pedro Vinicius <pvinicius2005@hotmail.com>
2025-03-05 12:24:23 -0300

    add attribute createdAt to Observation entity

--- src/main/java/com/pedroribeiro/helpaai/entities/Observation.java
+++ src/main/java/com/pedroribeiro/helpaai/entities/Observation.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Table(name = "tb_observacao")
 @Entity
 @AllArgsConstructor
@@ -19,6 +21,8 @@ public class Observation {
     @Column(columnDefinition = "TEXT",name = "observacao",nullable = false)
     private String observation;
 
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE",name = "criado_em",nullable = false)
+    private LocalDateTime createdAt;
 
     @ManyToOne
     @JoinColumn(name = "id_chamado")
