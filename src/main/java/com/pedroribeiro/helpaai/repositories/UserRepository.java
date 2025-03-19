33eb47e650676edb31aae728f1169c515a4dd64c
Pedro Vinicius <pvinicius2005@hotmail.com>
2025-03-19 16:54:16 -0300

    creating method to find user by email and return userdetails

--- src/main/java/com/pedroribeiro/helpaai/repositories/UserRepository.java
+++ src/main/java/com/pedroribeiro/helpaai/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.pedroribeiro.helpaai.repositories;
 
 import com.pedroribeiro.helpaai.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
 
+    UserDetails findByEmail(String email);
 }
