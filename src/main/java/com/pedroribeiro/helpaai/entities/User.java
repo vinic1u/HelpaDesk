ea9df5e244f25bcdb06f5d0d83844b9c62a97904
Pedro Vinicius <pvinicius2005@hotmail.com>
2025-03-19 16:53:57 -0300

    Implementing userdetails security in User entity

--- src/main/java/com/pedroribeiro/helpaai/entities/User.java
+++ src/main/java/com/pedroribeiro/helpaai/entities/User.java
@@ -1,12 +1,18 @@
 package com.pedroribeiro.helpaai.entities;
 
 
+import com.pedroribeiro.helpaai.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Table(name = "tb_usuario")
@@ -14,7 +20,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class User {
+public class User  implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,10 +32,57 @@ public class User {
     @Column(name = "email",nullable = false,length = 100,unique = true)
     private String email;
 
+    @Column(name = "senha",nullable = false,columnDefinition = "TEXT")
+    private String password;
+
     @Column(name = "telefone",nullable = false,length = 20,unique = true)
     private String phone;
+
+    private UserRole role;
 
     @ManyToOne
     @JoinColumn(name = "id_setor")
     private Sector sector;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN){
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_OPERATOR"),
+                    new SimpleGrantedAuthority("ROLE_CLIENT"));
+        }
+        if(this.role == UserRole.OPERATOR){
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_OPERATOR"),
+                    new SimpleGrantedAuthority("ROLE_CLIENT")
+            );
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_CLIENT"));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
