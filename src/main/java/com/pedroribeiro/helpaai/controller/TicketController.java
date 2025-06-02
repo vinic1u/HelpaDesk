bfc7dd6923788065cd6553051db0679ba04debbe
Pedro Vinicius <pvinicius2005@hotmail.com>
2025-06-02 18:43:38 -0300

    Add Ticket Operations control with Authentication context

--- src/main/java/com/pedroribeiro/helpaai/controller/TicketController.java
+++ src/main/java/com/pedroribeiro/helpaai/controller/TicketController.java
@@ -3,11 +3,13 @@ package com.pedroribeiro.helpaai.controller;
 import com.pedroribeiro.helpaai.dtos.observation.ObservationRequestDTO;
 import com.pedroribeiro.helpaai.dtos.ticket.TicketRequestDTO;
 import com.pedroribeiro.helpaai.dtos.ticket.TicketResponseDTO;
+import com.pedroribeiro.helpaai.entities.User;
 import com.pedroribeiro.helpaai.services.TicketService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,40 +24,44 @@ public class TicketController {
 
 
     @GetMapping
-    public ResponseEntity<List<TicketResponseDTO>> findAllTickets(){
-        return ResponseEntity.ok(ticketService.findAllTickets());
+    public ResponseEntity<List<TicketResponseDTO>> findAllTickets(@AuthenticationPrincipal User user){
+        return ResponseEntity.ok(ticketService.findAllTickets(user));
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<TicketResponseDTO> findTicketById(
+            @AuthenticationPrincipal User user,
             @PathVariable("id") Integer id
     ){
-        return ResponseEntity.ok(ticketService.findTicketById(id));
+        return ResponseEntity.ok(ticketService.findTicketById(user,id));
     }
 
     @PostMapping
     public ResponseEntity<TicketResponseDTO> saveTicket(
+            @AuthenticationPrincipal User user,
             @Valid @RequestBody TicketRequestDTO dto
             ){
-        TicketResponseDTO response = ticketService.saveTicket(dto);
+        TicketResponseDTO response = ticketService.saveTicket(user,dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PutMapping(value = "/{id}")
     public ResponseEntity<TicketResponseDTO> updateTicket(
+            @AuthenticationPrincipal User user,
             @PathVariable("id") Integer id,
             @Valid @RequestBody TicketRequestDTO dto
     ){
-        TicketResponseDTO response = ticketService.updateTicket(id,dto);
+        TicketResponseDTO response = ticketService.updateTicket(user,id,dto);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping(value = "/{id}/observation")
     public ResponseEntity<TicketResponseDTO> createObservation(
+            @AuthenticationPrincipal User user,
             @PathVariable("id") Integer id,
             @Valid @RequestBody ObservationRequestDTO dto
             ){
-        TicketResponseDTO response = ticketService.createObservation(id,dto);
+        TicketResponseDTO response = ticketService.createObservation(user,id,dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
