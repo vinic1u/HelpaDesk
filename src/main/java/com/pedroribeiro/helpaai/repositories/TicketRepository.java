effcf1698632507fc085afec78c552851a09c4c0
Pedro Vinicius <pvinicius2005@hotmail.com>
2025-06-02 15:39:05 -0300

    add query to find client tickets by email in token

--- src/main/java/com/pedroribeiro/helpaai/repositories/TicketRepository.java
+++ src/main/java/com/pedroribeiro/helpaai/repositories/TicketRepository.java
@@ -2,8 +2,16 @@ package com.pedroribeiro.helpaai.repositories;
 
 import com.pedroribeiro.helpaai.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TicketRepository  extends JpaRepository<Ticket,Integer> {
+
+    @Query(
+            value = "SELECT t FROM Ticket t WHERE t.client.email = ?1"
+    )
+    List<Ticket> findClientTickets(String email);
 }
